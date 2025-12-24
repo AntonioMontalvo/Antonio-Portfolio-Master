4421c191a4f7df3e64f203f5e5586ba26ad3da58
Antonio Montalvo <yocodeyo@icloud.com>
2025-12-23 17:21:01 -0800

    Add DataViz frontend URL to CORS whitelist
    - Allow https://antonio-dataviz-app.vercel.app origin

--- DataVizApp/server/app.py
+++ DataVizApp/server/app.py
@@ -19,6 +19,7 @@ CORS(app, origins=[
     "http://localhost:5173",
     "http://localhost:5174",
     "https://antonio-portfolio-master.vercel.app",
+    "https://antonio-dataviz-app.vercel.app",
     "https://*.vercel.app"
 ]) 
 
