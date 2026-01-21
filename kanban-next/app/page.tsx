33a8b78f9ce099b9b5fa39cd194cc3728b49ed1d
Antonio Montalvo <yocodeyo@icloud.com>
2026-01-21 15:31:53 -0800

    Remove kanban-next from tracking - now managed as separate repo

--- kanban-next/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Board } from "@/components/Board";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-100 py-8">
-      <Board />
-    </main>
-  );
-}
